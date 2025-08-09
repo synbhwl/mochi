ae7dfadd6762bf333ff2e027f415292ef9f81e5a
synbhwl <sayanbhowalemail@gmail.com>
2025-08-09 02:00:18 -0700

    added error handling and validators

--- core/database.py
+++ core/database.py
@@ -1,17 +1,10 @@
 from sqlmodel import SQLModel, create_engine, Field, Session, select, func  # noqa
 from typing import Optional
 
-# url_table holds the data regarding the core logic of the app,
-# namely shortening and redirection
-
-
 class Url_table(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     long: str
     code: Optional[str] = Field(default=None, unique=True, index=True)
-
-# clicks holds every click, use of a shortened URL mainly for the pupose of analytics
-
 
 class Clicks(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
--- routers/core_routes.py
+++ routers/core_routes.py
@@ -5,25 +5,35 @@ from fastapi import (
     Query,
     APIRouter
 )
-from fastapi.responses import RedirectResponse, HTMLResponse, Response
+from fastapi.responses import RedirectResponse, Response
 from pydantic import BaseModel, Field
 from typing import Optional
 import secrets
-from core.config import get_settings
 from datetime import datetime
 from urllib.parse import urlparse
 import json
+import os
+from dotenv import load_dotenv
+import logging
 
 from core.database import Url_table, Clicks, create_session
 
 from sqlmodel import SQLModel, create_engine, Field, Session, select, func  # noqa
-settings = get_settings()
+
+load_dotenv()
 router = APIRouter()
 
-base_url = settings.base_url
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+base_url = os.getenv("BASE_URL").strip()
+if not base_url:
+    logger.error("err: base_url missing in .env")
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="err: base_url missing")
 
 
-# url_req parses the url from the body of the post request
 class Url_req(BaseModel):
     long: str
 
@@ -32,28 +42,40 @@ def generate_code(length: int = 6) -> str:
     code = str(secrets.token_urlsafe(length))[:length]
     return code
 
-# requires json body
-
 
 @router.post('/shorten')
 async def shorten_url(
-    url: Url_req,  # takes the whole url as an object instance of url_req
+    url: Url_req,
     session: Session = Depends(create_session)
 ):
+    if not url:
+        logger.error("err: missing field in request body 'url'")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="""err: missing field in request body 'url',
+            eg: '{"url":"your url"}'""")
+
     code = generate_code()
     newURL = Url_table(
         long=url.long,
         code=code
     )
-
-    session.add(newURL)
-    session.commit()
-    session.refresh(newURL)
+    try:
+        session.add(newURL)
+        session.commit()
+        session.refresh(newURL)
+    except Exception as e:
+        logger.error(
+            f"err: database error while trying to add new URL: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"err: database error while trying to add new URL: {str(e)}")
 
     return {"new_url": f"this is you url {base_url}/{code}"}
 
 # violating rest api principles to make a direct shortener
-# i made it so the user can type the url in the address bar along with the query param
+# i made it so the user can type the url in the address bar
+# along with the query param
 # and directly get a shortened link with needing any further frontend or forms
 # making it accessible even to mobile users
 
@@ -63,12 +85,9 @@ async def shorten_url(
 # and have avoided excessive modularization of it
 
 
-@router.get('/', response_class=HTMLResponse)
+@router.get('/')
 async def direct_shortener(
-    # the url query parameter is an optional param with default as none
     url: Optional[str] = Query(None),
-    # if the param is present, it executes the shortening logic
-    # otherwise takes you to a home page
     session: Session = Depends(create_session)
 ):
     if url:
@@ -77,38 +96,51 @@ async def direct_shortener(
             long=url,
             code=code
         )
-
-        session.add(newURL)
-        session.commit()
-        session.refresh(newURL)
+        try:
+            session.add(newURL)
+            session.commit()
+            session.refresh(newURL)
+        except Exception as e:
+            logger.error(
+                f"err: database error while trying to add new URL:{str(e)}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"err: database error while trying to add new URL: {str(e)}")
 
         short_url = f"{base_url}/{code}"
-        with open('templates/shortened.html', 'r') as f:
-            html_content = f.read()
-        html_content = html_content.replace("{{short_url}}", short_url)
-        return HTMLResponse(content=html_content)
+        return {"short_url": short_url}
     else:
-        with open('templates/home.html', 'r') as f:
-            home_content = f.read()
-        home_content = home_content.replace("{{base_url}}", base_url)
-
-        return HTMLResponse(content=home_content)
+        return "welcome to mochi"
 
 
 # this route returns a basic analytics json schema for now
 @router.get('/analytics')
 async def show_analytics(url: str, session: Session = Depends(create_session)):
-    # this parses the path from the url and remove the '/'
+    if not url:
+        logger.error("err: missing query parameter 'url'")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="err: missing query parameter 'url', eg: /analytics?url=your_url")
+
     code = urlparse(url).path.strip('/')
+    if not code:
+        logger.error("err: code missing at the end of shortened url")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="err: code missing at the end of shortened url, eg: /U4q0dX")
+    try:
+        clicks = session.exec(select(func.count()).where(
+            Clicks.url_code == code)).first()
 
-    clicks = session.exec(select(func.count()).where(
-        Clicks.url_code == code)).first()  # counts the number of clicks
+        history = session.exec(select(Clicks).where(
+            Clicks.url_code == code)).all()
+    except Exception as e:
+        logger.error(
+            f"err: either total_clicks or timestamp_history doesn't exist: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"err: either total_clicks or timestamp_history doesn't exist: {str(e)}")
 
-    history = session.exec(select(Clicks).where(
-        Clicks.url_code == code)).all()  # returns result object
-    # of all the Clicks that had the particular code
-
-    # takes out only the time stamps
     timestamps = [t.timestamp for t in history]
 
     analytics = {
@@ -117,13 +149,9 @@ async def show_analytics(url: str, session: Session = Depends(create_session)):
         "time_history": timestamps
     }
     return Response(
-        content=json.dumps(analytics, indent=2),  # this is for pretty print
+        content=json.dumps(analytics, indent=2),
         media_type="application/json"
     )
-
-# this route handles the rediction of the user +
-# the incrementation logic of the counting feature
-# it adds a click object everytime a the shortened link is clicked/used
 
 
 @router.get('/{code}')
@@ -131,16 +159,28 @@ async def redirect(
     code: str,
     session: Session = Depends(create_session)
 ):
+    if not code:
+        logger.error("err: missing path parameter 'code'")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="err: missing path parameter 'code', eg: /U4q0dX")
+
     result = session.exec(select(Url_table).where(
         Url_table.code == code)).first()
     if result is None:
+        logger.error("err: no such url found in the database")
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="no such url found")
+            status_code=status.HTTP_404_NOT_FOUND, detail="err: no such url found in the database")
 
     time = datetime.utcnow().isoformat()
     click = Clicks(timestamp=time, url_code=code)
-    session.add(click)
-    session.commit()
-    session.refresh(click)
+    try:
+        session.add(click)
+        session.commit()
+        session.refresh(click)
+    except Exception as e:
+        logger.error(
+            f"err: database error while trying to add new click:{str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"err: database error while trying to add new click: {str(e)}")
 
     return RedirectResponse(result.long)
